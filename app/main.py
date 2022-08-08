c1e28caff4fae3a8dfcf0a66d5867e254f13b229
Zac Pullar-Strecker <zacmps@gmail.com>
2022-08-08 19:02:03 +1200

    Add rotten tomatoes details

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import html
 from datetime import datetime, timedelta
+from functools import lru_cache
 
 from flask import Flask, Response
 from ics import Calendar, Event
@@ -7,6 +8,8 @@ import requests
 import bs4
 import js2py
 import pytz
+
+from rottentomatoes import rottentomatoes as rt
 
 
 app = Flask(__name__)
@@ -28,26 +31,41 @@ def get_data():
     return parsed_releases
 
 
+@lru_cache
+def rotten_tomatoes_details(name: str) -> str:
+    try:
+        return str(rt.Film(name))
+    except Exception as e:
+        print(e)
+        return "Failed to fetch details from rotten tomatoes"
+
+
 def build_calendar(parsed_releases):
     cal = Calendar()
     timezone = pytz.timezone('Pacific/Auckland')
-    
+
     for date, films in parsed_releases.items():
         for film in films:
             for time in film['times']:
-                start_time = datetime.strptime(f"{date} {time['time']}", "%Y-%m-%d %H:%M%p")
-                start_time.replace(tzinfo=timezone)
-                duration = timedelta(minutes=int(film['duration'][:-5]))
-                event = Event(
-                    name=film['title'],
-                    begin=start_time,
-                    end=f"{start_time + duration}",
-                    location=f"Academy Cinemas",
-                    url=html.unescape(time['bookingLink']),
-                    categories=[f"FILM"],
-                    description=time['bookingLink']
-                )
-                cal.events.add(event)
+                try:
+                    details = rotten_tomatoes_details(film['title'])
+
+                    start_time = datetime.strptime(f"{date} {time['time']}", "%Y-%m-%d %H:%M%p")
+                    start_time.replace(tzinfo=timezone)
+                    duration = timedelta(minutes=int(film['duration'][:-5]))
+                    event = Event(
+                        name=film['title'],
+                        begin=start_time,
+                        end=f"{start_time + duration}",
+                        location=f"Academy Cinemas",
+                        url=html.unescape(time['bookingLink']),
+                        categories=[f"FILM"],
+                        description=f"{details}\n\n{html.unescape(time['bookingLink'])}"
+                    )
+                    cal.events.add(event)
+                except Exception as e:
+                    print(f"Failed to process details for film: {e}")
+                    print(film)
 
     return cal
 
